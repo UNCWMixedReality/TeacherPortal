3485d6b31add39f003d9646beb8fdcf592f287fa
SethAngell <accounts@sethangell.com>
2022-03-03 14:56:58 -0500

    Dockerfile.prod hotfix

--- .ci/build.sh
+++ .ci/build.sh
@@ -3,5 +3,5 @@
 cd .ci || cd /home/docker/TeacherPortal/.ci
 
 # build and deploy
-docker-compose --file prod.docker-compose.yaml build --build-arg S3_KEY=${S3_KEY} --build-arg S3_SECRET_KEY=${S3_SECRET_KEY};
+docker-compose --file prod.docker-compose.yaml build;
 docker-compose --file prod.docker-compose.yaml up -d;
